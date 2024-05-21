70d0a8444aeba63aff1a0474f94d56d942b72fd7
LAPTOP-RHN37B9N\User <yihuan981@gmail.com>
2024-05-21 18:05:27 +0800

    加入專案檔案。

---
+++ TaskPractice/Program.cs
@@ -0,0 +1,70 @@
+﻿using StreamLibarary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = "D:\\研究所檔案\\軟體開發學習與就業\\軟體開發家教課\\程式碼\\Sub11_AsyncAwait\\TaskPractice\\testData";
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int lineNum = 10000000;
+            int batchSize = 100000;
+            string fileName = "MOCK_DATA_10M";
+
+            ParallelizedBatch(lineNum, batchSize, dir, fileName).GetAwaiter().GetResult();
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+
+
+        static void SerializedBatch(int lineNum, int batchSize, string dir, string fileName)
+        {
+            int batchNum = lineNum / batchSize;
+
+            for (int i = 0; i < batchNum; i++)
+            {
+                var data = CsvHelper.ReadCSV<UserModel>($"{dir}\\{fileName}.csv", i * batchSize + 1, batchSize);
+                CsvHelper.WriteCSV($"{dir}\\{fileName}\\write_batch_{i}.csv", data, append: false, createDir: true);
+                GC.Collect();
+            }
+        }
+
+
+        static async Task ParallelizedBatch(int lineNum, int batchSize, string dir, string fileName)
+        {
+            List<Task> tasks = new List<Task>();
+
+            int batchNum = lineNum / batchSize;
+
+            // var let
+            for (int i = 0; i < batchNum; i++)
+            {
+                int fileIndex = i;
+                tasks.Add(Task.Run(() =>
+                {
+                    var data = CsvHelper.ReadCSV<UserModel>($"{dir}\\{fileName}.csv", fileIndex * batchSize + 1, batchSize);
+                    CsvHelper.WriteCSV($"{dir}\\{fileName}_parallel\\write_batch_{fileIndex}.csv", data, append: false, createDir: true);
+                    GC.Collect();
+                }));
+                //Thread.Sleep(1000);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ TaskPractice/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPractice
+{
+    internal class UserModel
+    {
+        //id,first_name,last_name,email,gender,ip_address
+
+        public string id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string ip_address { get; set; }
+
+    }
+}
